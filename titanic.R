4cde39ef2d4653af13247a50f42cac6f80a60984
mkay313 <mkmozgawa@gmail.com>
2018-04-07 18:39:28 +0200

    adds logistic regression models

--- titanic.R
+++ titanic.R
@@ -2,6 +2,7 @@
 
 library(Amelia)
 library(tidyverse)
+library(caTools)
 
 df_train <- read.csv("titanic_train.csv")
 df_test <- read.csv("titanic_test.csv")
@@ -9,6 +10,7 @@ df_test <- read.csv("titanic_test.csv")
 missmap(df_train, main = "Missing values in training data", col = c('red', 'black'), legend = FALSE)
 missmap(df_test, main = "Missing values in test data", col = c('red', 'black'), legend = FALSE)
 # loads of Age values missing in both train and test, single fare value missing from test
+# we'll try to fill those out after some more analysis
 
 # Survivors
 ggplot(data = df_train, aes(Survived)) + 
@@ -37,12 +39,12 @@ CalculateMeanAge <- function(age) {
 train_mean_ages <- vector(mode = "numeric", length = 3)
 for (i in 1:3) {
   train_age_per_class <- train_df_set_age %>%
-    filter(train_df_set_age$Pclass == i)
+    filter(Pclass == i)
   train_mean_ages[i] <- CalculateMeanAge(train_age_per_class$Age)
 }
 
 AddMissingAge <- function(ages, class, mean_ages) {
-  this_ages <- vector(mode = "numeric", length = length(ages))
+  this_age <- vector(mode = "numeric", length = length(ages))
   for (i in 1:length(ages)) {
     if (is.na(ages[i])) {
       if (class[i] == 1) {
@@ -59,3 +61,51 @@ AddMissingAge <- function(ages, class, mean_ages) {
 }
 
 df_train$Age <- AddMissingAge(df_train$Age, df_train$Pclass, train_mean_ages)
+
+test_df_set_age <- df_test %>%
+  filter(!(is.na(Age)))
+
+test_mean_ages <- vector(mode = "numeric", length = 3)
+for (i in 1:3) {
+  test_age_per_class <- test_df_set_age %>%
+    filter(Pclass == i)
+  test_mean_ages[i] <- CalculateMeanAge(test_age_per_class$Age)
+}
+
+df_test$Age <- AddMissingAge(df_test$Age, df_test$Pclass, test_mean_ages)
+
+df_train <- df_train %>%
+  select(-PassengerId, -Name, -Ticket, -Cabin)
+
+df_train$Survived <- factor(df_train$Survived)
+df_train$Pclass <- factor(df_train$Pclass)
+df_train$SibSp <- factor(df_train$SibSp)
+df_train$Parch <- factor(df_train$Parch)
+
+log_model <- glm(Survived ~ ., family = binomial(link = "logit"),
+                 data = df_train)
+print(summary(log_model))
+
+# first check the model fit
+split <- sample.split(df_train$Survived, SplitRatio = 0.7)
+final_train <- subset(df_train, split == TRUE)
+final_test <- subset(df_train, split == FALSE)
+final_log_model <- glm(Survived ~ ., family = binomial(link = 'logit'), data = final_train)
+print(summary(final_log_model))
+fitted_probabilities <- predict(final_log_model, final_test, type = "response")
+fitted_results <- ifelse(fitted_probabilities > 0.5, 1, 0)
+summary(fitted_results)
+misClassError <- mean(fitted_results != final_test$Survived)
+print(table(final_test$Survived, fitted_probabilities > 0.5))
+
+# df_test$Pclass <- factor(df_test$Pclass)
+# df_test$SibSp <- factor(df_test$SibSp)
+# 
+# # there are 2 people with 9 Parch in test which leaves us with more factors than needed
+# # lets replace those 9s with 6s
+# df_test$Parch[df_test$Parch==9] <- 6
+# df_test$Parch <- factor(df_test$Parch)
+# 
+# fitted_probabilities <- predict(log_model, df_test, type = 'response')
+# fitted_results <- ifelse(fitted_probabilities > 0.5, 1, 0)
+# print(summary(fitted_results))
