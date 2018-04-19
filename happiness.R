ce8bc55c699a287c0564bc33fa96b79aa06c94b0
mkay313 <mkmozgawa@gmail.com>
2018-04-19 23:09:50 +0200

    initial commit

---
+++ happiness.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+library(Amelia)
+
+# data source: https://www.kaggle.com/unsdsn/world-happiness
+
+# read in the data and add the year column
+h_2015 <- read.csv("2015.csv", header = TRUE) %>%
+  mutate(year = 2015)
+h_2016 <- read.csv("2016.csv", header = TRUE) %>%
+  mutate(year = 2016)
+h_2017 <- read.csv("2017.csv", header = TRUE) %>%
+  mutate(year = 2017)
+
+h_2015 <- h_2015 %>%
+  select(-Standard.Error)
+
+h_2016 <- h_2016 %>%
+  select(-c(Lower.Confidence.Interval, Upper.Confidence.Interval))
+
+h_2017 <- h_2017 %>%
+  select(-c(Whisker.high, Whisker.low))
+
+# 2017 is missing the regions -- lets add them
+regions <- h_2015[match(h_2017$Country, h_2015$Country), "Region"]
+h_2017$Region <- regions
+
+# there are some NAs, lets address that
+print(is.na(h_2017$Region))
+v <- 0
+for (i in 1:nrow(h_2017)) {
+  if (is.na(h_2017$Region[i])) {
+    v <- c(v, i)
+  }
+}
+print(h_2017$Country[v])
+h_2017$Region[v[2]] <- "Eastern Asia"
+h_2017$Region[v[3]] <- "Latin America and Caribbean"
+h_2017$Region[v[4]] <- "Eastern Asia"
+h_2017$Region[v[5]] <- "Sub-Saharan Africa"
+h_2017$Region[v[6]] <- "Sub-Saharan Africa"
+h_2017$Region[v[7]] <- "Sub-Saharan Africa"
+print(is.na(h_2017$Region))
+
+hap <- rbind(h_2015, h_2016, h_2017)
