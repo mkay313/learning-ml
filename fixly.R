93c24bcc101d5dc0610f02c474b9f621bba2dc16
mkay313 <mkmozgawa@gmail.com>
2018-05-12 14:44:42 +0200

    minor reformat

--- fixly.R
+++ fixly.R
@@ -1,4 +1,5 @@
 library(rvest)
+library(tidyverse)
 
 # wyciągamy linki do poszczególnych kategorii
 kategorie_page <- read_html("https://fixly.pl/kategorie")
@@ -9,7 +10,7 @@ kategorie <- linki[sapply(linki, function(x) grepl("https://fixly.pl/kategoria/"
 kategorie_ile <- length(kategorie)
 
 #nazwy kategorii
-kategorie_nazwy <- unlist(lapply(kategorie, function(x) gsub("https://fixly.pl/kategoria/", "", x)))
+kategorie_nazwy <- map_chr(kategorie, function(x) gsub("https://fixly.pl/kategoria/", "", x))
 
 # wyciągamy wykonawców z poszczególnych kategorii
 wykonawcy_wg_kategorii <- vector(mode = "list", length = kategorie_ile)
@@ -20,7 +21,7 @@ for (i in 1:kategorie_ile) {
     html_nodes("a") %>%
     html_attr("href")
   wykonawcy_w_kategorii_page <- wykonawcy_w_kategorii_page[sapply(wykonawcy_w_kategorii_page, function(x) grepl("https://fixly.pl/profil/",x)) == TRUE]
-  wykonawcy_wg_kategorii[[i]] <- unlist(lapply(wykonawcy_w_kategorii, function(x) gsub("#feedback", "", x)))
+  wykonawcy_wg_kategorii[[i]] <- map_chr(wykonawcy_w_kategorii_page, function(x) gsub("#feedback", "", x))
   Sys.sleep(sample(seq(0.1,1,0.1),1))
 }
 
@@ -28,7 +29,3 @@ for (i in 1:kategorie_ile) {
 # możemy śmiało odlistować wszystkie dane -- pozyskamy potem informację o wszystkich miejscach, w których znajduje się wykładowca
 # samo info, na której kategorii znaleźliśmy danego wykonawcę też zapiszemy -- może coś nam powie jak są wybierani wykonawcy pokazywani na podstronach?
 
-wykonawcy_link_do_profilu <- unlist(wykonawcy_wg_kategorii, use.names = FALSE)
-wykonawcy_nazwy <- unlist(lapply(wykonawcy_link_do_profilu, function(x) gsub("https://fixly.pl/profil/", "", x)))
-wykonawcy_ile <- length(wykonawcy_nazwy)
-wykonawcy <- vector(mode = "list", length = wykonawcy_ile)
